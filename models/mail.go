d3b0b1f4acce9de6666559f4b444f9113832105d
Julian Weise <privat@julianweise.de>
2018-06-16 18:01:29 +0200

    add json parse information to model

--- models/mail.go
+++ models/mail.go
@@ -1,10 +1,10 @@
 package models
 
 type Mail struct {
-	Received 		string
-	ReceivedFrom 	string
-	ReceivedBy		string
-	MailFrom		string
-	RCPTTo			string
-	Data			string
+	Received 		string 	`json:"received,omitempty"`
+	ReceivedFrom 	string 	`json:"received_from,omitempty"`
+	ReceivedBy		string	`json:"received_by,omitempty"`
+	MailFrom		string	`json:"mail_from,omitempty"`
+	RCPTTo			string	`json:"rcpt_to,omitempty"`
+	Data			string	`json:"data,omitempty"`
 }
