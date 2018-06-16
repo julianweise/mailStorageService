817f6c60fab8a08eb22340e12b13e8c0733201f7
Julian Weise <privat@julianweise.de>
2018-06-16 18:02:15 +0200

    add IsValid method to Mail model

--- models/mail.go
+++ models/mail.go
@@ -1,5 +1,7 @@
 package models
 
+import "errors"
+
 type Mail struct {
 	Received 		string 	`json:"received,omitempty"`
 	ReceivedFrom 	string 	`json:"received_from,omitempty"`
@@ -7,4 +9,26 @@ type Mail struct {
 	MailFrom		string	`json:"mail_from,omitempty"`
 	RCPTTo			string	`json:"rcpt_to,omitempty"`
 	Data			string	`json:"data,omitempty"`
+}
+
+func (mail* Mail) IsValid() error {
+	if mail.Received == "" {
+		return errors.New("receive should not be empty")
+	}
+	if mail.ReceivedFrom == "" {
+		return errors.New("received_from should not be empty")
+	}
+	if mail.ReceivedBy == "" {
+		return errors.New("received_by should not be empty")
+	}
+	if mail.MailFrom == "" {
+		return errors.New("mail_from should not be empty")
+	}
+	if mail.RCPTTo == "" {
+		return errors.New("rcpt_to should not be empty")
+	}
+	if mail.Data == "" {
+		return errors.New("data should not be empty")
+	}
+	return nil
 }
